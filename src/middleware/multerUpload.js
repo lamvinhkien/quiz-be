81a2fd031c0c32a7d699bc80f8eebe403cb7395c
lamvinhkien <lamvinhkien1709@gmail.com>
2026-05-30 23:25:19 +0700

    updateeee

--- src/middleware/multerUpload.js
+++ src/middleware/multerUpload.js
@@ -1,8 +1,17 @@
 const path = require('path')
 const multer = require('multer')
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'src/public/uploads/')
+        let uploadDir;
+
+        if (__dirname.includes('build')) {
+            uploadDir = path.join(process.cwd(), 'public', 'uploads');
+        } else {
+            uploadDir = 'src/public/uploads/';
+        }
+
+        cb(null, uploadDir)
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname)
