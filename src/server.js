39e1f69bb43da8fcc7c44ed886adb5b9ca52c44d
lamvinhkien <lamvinhkien1709@gmail.com>
2026-05-30 23:06:14 +0700

    update uploads url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const initQuizRoutes = require('./routes/QuizRoute.js');
 const initAdminRoutes = require('./routes/AdminRoute.js');
 const initQuestionRoutes = require('./routes/QuestionRoute.js');
@@ -11,7 +12,24 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const app = express()
 
-app.use('/uploads', express.static(path.join(__dirname, './public/uploads')));
+let uploadDir;
+
+
+if (__dirname.includes('build')) {
+    uploadDir = path.join(process.cwd(), 'public', 'uploads');
+    console.log('>>> 🚀 Đang chạy trên RENDER. Thư mục uploads:', uploadDir);
+} else {
+    uploadDir = path.join(__dirname, './public/uploads');
+    console.log('>>> 💻 Đang chạy dưới LOCAL. Thư mục uploads:', uploadDir);
+}
+
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log('>>> 🎉 Đã tự động tạo thư mục public/uploads tại:', uploadDir);
+}
+
+app.use('/uploads', express.static(uploadDir));
 
 configCors(app)
 
@@ -26,6 +44,8 @@ initQuestionRoutes(app)
 initAnswerRoutes(app)
 initCategoryRoutes(app)
 
-app.listen(process.env.PORT, () => {
-  console.log('>>> Server running on PORT:', process.env.PORT)
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log('>>> 🖥️ Server đang chạy tại PORT:', PORT)
 })
